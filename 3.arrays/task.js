fd863b0894d7ba234954c704bd0d67514e795476
Bilen Liubov <bilenliubov48@mail.ru>
2025-03-05 13:13:45 +0300

    Первое задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	return arr1.every((value, index) => {
+		console.log(value, arr2[index]);
+		return value === arr2[index];
+	})
 }
 
+
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let filterCount = 0;
+	let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, ageItem) => acc + ageItem.age, 0) / filterCount;
+	if (users.filter(user => user.gender === gender).length === 0) {
+		return 0;
+	}
+	return result;
 }
+
