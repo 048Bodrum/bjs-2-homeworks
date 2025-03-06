31ee14034896097de338ba8d9d62f143022498c6
Bilen Liubov <bilenliubov48@mail.ru>
2025-03-06 13:01:19 +0300

    Задание полностью выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (!this.marks) {
+		console.log("Студент отчислен");
+		return;
+	}
+	if (!!marks.length) {
+		this.marks.push(...marks);
+	}
+	return;
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || !this.marks.length) {
+		return 0;
+	}
+	const sum = this.marks.reduce((acc, current) => acc + current);
+	return sum / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
+
 }
