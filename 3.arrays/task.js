fc152bf6e92a14115037c92e4d320d62d9d1011f
Bilen Liubov <bilenliubov48@mail.ru>
2025-03-10 21:53:29 +0300

    Доработано второе задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -11,8 +11,7 @@ function compareArrays(arr1, arr2) {
 
 
 function getUsersNamesInAgeRange(users, gender) {
-	let filterCount = 0;
-	let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, ageItem) => acc + ageItem.age, 0) / filterCount;
+	let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item) => acc + item, 0) / users.filter(user => user.gender === gender).length;
 	if (users.filter(user => user.gender === gender).length === 0) {
 		return 0;
 	}
